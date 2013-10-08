546503812de3e79fb860e96bc89c8db8b7206ca5
shyamak <shyamakaggarwal@yahoo.com>
2013-10-08 00:09:20 -0400

    Updated. (Shyamak & Tanay) Second screen.

--- M5/src/PlayerConfigView.java
+++ M5/src/PlayerConfigView.java
@@ -1,1 +1,339 @@
-// put your shit here shayamak & tanay thx
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+
+public class PlayerScreen {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PlayerScreen window = new PlayerScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PlayerScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(255, 239, 213));
+		frame.setBounds(100, 100, 850, 549);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JLabel lblMule = new JLabel("");
+		lblMule.setIcon(new ImageIcon("C:\\Users\\Tanay\\workspace\\MuleGame\\src\\mule_mini.jpg"));
+		lblMule.setFont(new Font("Times New Roman", Font.BOLD, 19));
+		lblMule.setHorizontalAlignment(SwingConstants.LEFT);
+		lblMule.setBounds(60, 11, 77, 76);
+		frame.getContentPane().add(lblMule);
+		
+		JLabel lblMule_1 = new JLabel("M.U.L.E.");
+		lblMule_1.setBackground(new Color(0, 204, 153));
+		lblMule_1.setForeground(new Color(255, 0, 204));
+		lblMule_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMule_1.setFont(new Font("Times New Roman", Font.BOLD, 35));
+		lblMule_1.setBounds(268, 11, 256, 76);
+		frame.getContentPane().add(lblMule_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 105, 398, 135);
+		frame.getContentPane().add(panel);
+				panel.setLayout(null);
+		
+		JLabel lblSelectPlayerName = new JLabel("Select Player Name:");
+		lblSelectPlayerName.setBounds(10, 11, 104, 16);
+		panel.add(lblSelectPlayerName);
+		lblSelectPlayerName.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		
+		textField = new JTextField();
+		textField.setBounds(163, 9, 130, 20);
+		panel.add(textField);
+		textField.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		textField.setColumns(10);
+		
+		JLabel lblMaxCharacters = new JLabel("(Max. 25 characters)");
+		lblMaxCharacters.setBounds(296, 12, 101, 14);
+		panel.add(lblMaxCharacters);
+		lblMaxCharacters.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+								
+		JLabel lblSelectPlayerRace = new JLabel("Select Player Race:");
+		lblSelectPlayerRace.setBounds(10, 46, 121, 14);
+		panel.add(lblSelectPlayerRace);
+		lblSelectPlayerRace.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+										
+		final JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(163, 40, 130, 20);
+		panel.add(comboBox);
+		comboBox.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Humanoid", "Ponkshe", "Lall", "Flapper"}));
+												
+		JLabel lblSelectPlayerColor = new JLabel("Select Player Color:");
+		lblSelectPlayerColor.setBounds(10, 82, 121, 14);
+		panel.add(lblSelectPlayerColor);
+		lblSelectPlayerColor.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+														
+		final JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(163, 79, 130, 20);
+		panel.add(comboBox_1);
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Red", "Blue", "Green", "Yellow", "Pink", "Orange", "Cyan", "Maroon"}));
+		comboBox_1.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																
+															
+		JLabel lblNoOfResources = new JLabel("Amount available at start:");
+		lblNoOfResources.setBounds(10, 110, 137, 14);
+		panel.add(lblNoOfResources);
+		lblNoOfResources.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																		
+		final JLabel lblNewLabel = new JLabel("600");
+		lblNewLabel.setBounds(163, 110, 130, 14);
+		panel.add(lblNewLabel);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+																				
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(96, 397, 457, 46);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+												
+		JButton btnClear = new JButton("<< Back");
+		btnClear.setBounds(58, 11, 93, 25);
+		panel_1.add(btnClear);
+		btnClear.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																
+		JButton btnClear_1 = new JButton("Clear");
+		btnClear_1.setBounds(161, 12, 83, 23);
+		panel_1.add(btnClear_1);
+																							
+		JButton btnNext = new JButton("Next >>");
+		btnNext.setBounds(254, 12, 77, 23);
+		panel_1.add(btnNext);
+																									
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(341, 12, 84, 23);
+		panel_1.add(btnExit);
+																											
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(10, 454, 806, 48);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+																														
+		JLabel lblNewLabel_1 = new JLabel(" \u00A9 Shyamak Aggarwal, Tanay Ponkshe, Nikhil Karajgikar, Jerry Yee & Ishaan Grover ");
+		lblNewLabel_1.setBounds(10, 23, 407, 14);
+		panel_2.add(lblNewLabel_1);
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+																																
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(null);
+		panel_3.setBounds(418, 105, 398, 135);
+		frame.getContentPane().add(panel_3);
+																																
+		JLabel label = new JLabel("Select Player Name:");
+		label.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label.setBounds(10, 11, 104, 16);
+		panel_3.add(label);
+																															
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		textField_1.setColumns(10);
+		textField_1.setBounds(163, 9, 130, 20);
+		panel_3.add(textField_1);
+																																
+		JLabel label_1 = new JLabel("(Max. 25 characters)");
+		label_1.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		label_1.setBounds(296, 12, 101, 14);
+		panel_3.add(label_1);
+																																
+		JLabel label_2 = new JLabel("Select Player Race:");
+		label_2.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label_2.setBounds(10, 46, 121, 14);
+		panel_3.add(label_2);
+																							
+		final JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Humanoid", "Ponkshe", "Lall", "Flapper"}));
+		comboBox_2.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		comboBox_2.setBounds(163, 40, 130, 20);
+		panel_3.add(comboBox_2);
+																																
+		JLabel label_3 = new JLabel("Select Player Color:");
+		label_3.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label_3.setBounds(10, 82, 121, 14);
+		panel_3.add(label_3);
+																																
+		final JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"Red", "Blue", "Green", "Yellow", "Pink", "Orange", "Cyan", "Maroon"}));
+		comboBox_3.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		comboBox_3.setBounds(163, 79, 130, 20);
+		panel_3.add(comboBox_3);
+																															
+		JLabel label_4 = new JLabel("Amount available at start:");
+		label_4.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label_4.setBounds(10, 110, 137, 14);
+		panel_3.add(label_4);
+																										
+		JLabel label_5 = new JLabel("600");
+		label_5.setHorizontalAlignment(SwingConstants.CENTER);
+		label_5.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label_5.setBounds(163, 110, 130, 14);
+		panel_3.add(label_5);
+																															
+		JPanel panel_4 = new JPanel();
+		panel_4.setLayout(null);
+		panel_4.setBounds(10, 251, 398, 135);
+		frame.getContentPane().add(panel_4);
+																												
+		JLabel label_6 = new JLabel("Select Player Name:");
+		label_6.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label_6.setBounds(10, 11, 104, 16);
+		panel_4.add(label_6);
+																					
+		textField_2 = new JTextField();
+		textField_2.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		textField_2.setColumns(10);
+		textField_2.setBounds(163, 9, 130, 20);
+		panel_4.add(textField_2);
+																										
+		JLabel label_7 = new JLabel("(Max. 25 characters)");
+		label_7.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+		label_7.setBounds(296, 12, 101, 14);
+		panel_4.add(label_7);
+							
+		JLabel label_8 = new JLabel("Select Player Race:");
+		label_8.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label_8.setBounds(10, 46, 121, 14);
+									                                              panel_4.add(label_8);
+																																
+																																final JComboBox comboBox_4 = new JComboBox();
+																																comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"Humanoid", "Ponkshe", "Lall", "Flapper"}));
+																																comboBox_4.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																comboBox_4.setBounds(163, 40, 130, 20);
+																																panel_4.add(comboBox_4);
+																																
+																																JLabel label_9 = new JLabel("Select Player Color:");
+																																label_9.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																label_9.setBounds(10, 82, 121, 14);
+																																panel_4.add(label_9);
+																																
+																																final JComboBox comboBox_5 = new JComboBox();
+																																comboBox_5.setModel(new DefaultComboBoxModel(new String[] {"Red", "Blue", "Green", "Yellow", "Pink", "Orange", "Cyan", "Maroon"}));
+																																comboBox_5.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																comboBox_5.setBounds(163, 79, 130, 20);
+																																panel_4.add(comboBox_5);
+																																
+																																JLabel label_10 = new JLabel("Amount available at start:");
+																																label_10.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																label_10.setBounds(10, 110, 137, 14);
+																																panel_4.add(label_10);
+																																
+																																JLabel label_11 = new JLabel("600");
+																																label_11.setHorizontalAlignment(SwingConstants.CENTER);
+																																label_11.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																label_11.setBounds(163, 110, 130, 14);
+																																panel_4.add(label_11);
+																																
+																																JPanel panel_5 = new JPanel();
+																																panel_5.setLayout(null);
+																																panel_5.setBounds(418, 251, 398, 135);
+																																frame.getContentPane().add(panel_5);
+																																
+																																JLabel label_12 = new JLabel("Select Player Name:");
+																																label_12.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																label_12.setBounds(10, 11, 104, 16);
+																																panel_5.add(label_12);
+																																
+																																textField_3 = new JTextField();
+																																textField_3.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																textField_3.setColumns(10);
+																																textField_3.setBounds(163, 9, 130, 20);
+																																panel_5.add(textField_3);
+																																
+																																JLabel label_13 = new JLabel("(Max. 25 characters)");
+																																label_13.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+																																label_13.setBounds(296, 12, 101, 14);
+																																panel_5.add(label_13);
+																																
+																																JLabel label_14 = new JLabel("Select Player Race:");
+																																label_14.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																label_14.setBounds(10, 46, 121, 14);
+																																panel_5.add(label_14);
+																																
+																																final JComboBox comboBox_6 = new JComboBox();
+																																comboBox_6.setModel(new DefaultComboBoxModel(new String[] {"Humanoid", "Ponkshe", "Lall", "Flapper"}));
+																																comboBox_6.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																comboBox_6.setBounds(163, 40, 130, 20);
+																																panel_5.add(comboBox_6);
+																																
+																																JLabel label_15 = new JLabel("Select Player Color:");
+																																label_15.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																label_15.setBounds(10, 82, 121, 14);
+																																panel_5.add(label_15);
+																																
+																																final JComboBox comboBox_7 = new JComboBox();
+																																comboBox_7.setModel(new DefaultComboBoxModel(new String[] {"Red", "Blue", "Green", "Yellow", "Pink", "Orange", "Cyan", "Maroon"}));
+																																comboBox_7.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																comboBox_7.setBounds(163, 79, 130, 20);
+																																panel_5.add(comboBox_7);
+																																
+																																JLabel label_16 = new JLabel("Amount available at start:");
+																																label_16.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+																																label_16.setBounds(10, 110, 137, 14);
+																																panel_5.add(label_16);
+																																
+																																JLabel label_17 = new JLabel("600");
+		label_17.setHorizontalAlignment(SwingConstants.CENTER);
+		label_17.setFont(new Font("Times New Roman", Font.PLAIN, 13));
+		label_17.setBounds(163, 110, 130, 14);
+		panel_5.add(label_17);
+		btnExit.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+		}
+		});
+																								btnClear_1.addActionListener(new ActionListener() {
+																									public void actionPerformed(ActionEvent e) {
+																										textField.setText("");
+																										textField_1.setText("");
+																										textField_2.setText("");
+																										textField_3.setText("");
+																										comboBox.setSelectedIndex(0);
+																										comboBox_1.setSelectedIndex(0);
+																										comboBox_2.setSelectedIndex(0);
+																										comboBox_3.setSelectedIndex(0);
+																										comboBox_4.setSelectedIndex(0);
+																										comboBox_5.setSelectedIndex(0);
+																										comboBox_6.setSelectedIndex(0);
+																										comboBox_7.setSelectedIndex(0);
+																										lblNewLabel.setText("600");
+																									}
+																								});
+																						btnClear.addActionListener(new ActionListener() {
+																							public void actionPerformed(ActionEvent arg0) {
+																							}
+																						});
+												int s = comboBox.getSelectedIndex();
+	}
+}
